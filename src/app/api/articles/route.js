36a3ac898d4026c5d770f1a8d818bddbcc001e20
halionaz <samhsj@naver.com>
2024-02-21 00:31:24 +0900

    ✨ Implement Next.js Server Side API Func

---
+++ src/app/api/articles/route.js
@@ -0,0 +1,11 @@
+import {NextResponse} from "next/server";
+
+export async function GET (request){
+    
+    const greeting = "Hello World!!"
+    const json = {
+        greeting
+    };
+    
+    return NextResponse.json(json);
+}
