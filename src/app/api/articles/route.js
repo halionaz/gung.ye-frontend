b4cf270884b6ba886684bf8cee31ee066e59bfcf
halionaz <samhsj@naver.com>
2024-02-21 00:43:38 +0900

    ✨ Implement Articles GET API

--- src/app/api/articles/route.js
+++ src/app/api/articles/route.js
@@ -1,11 +1,14 @@
-import {NextResponse} from "next/server";
+import { NextResponse } from "next/server";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../firebase";
 
-export async function GET (request){
+export async function GET(request) {
+    const querySnapshot = await getDocs(collection(db, "articles"));
+
+    const data = [];
+    querySnapshot.forEach((doc) => {
+        data.push({ ...doc.data(), id: doc.id });
+    });
     
-    const greeting = "Hello World!!"
-    const json = {
-        greeting
-    };
-    
-    return NextResponse.json(json);
+    return NextResponse.json(data);
 }
