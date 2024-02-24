70eb63758b2c6d59f532c8f38a1fb665ea509c07
halionaz <samhsj@naver.com>
2024-02-24 12:25:44 +0900

    ✨ Implement Credential Sign in

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,13 +1,43 @@
 // route.js는 Server-Side API를 위한 문서 작성용
 
 import NextAuth from "next-auth/next";
-import GithubProvider from "next-auth/providers/github";
+import CredentialsProvider from "next-auth/providers/credentials";
 
 const handler = NextAuth({
     providers: [
-        GithubProvider({
-            clientId: process.env.GITHUB_ID,
-            clientSecret: process.env.GITHUB_SECRET,
+        CredentialsProvider({
+            // The name to display on the sign in form (e.g. "Sign in with...")
+            name: "Credentials",
+            // `credentials` is used to generate a form on the sign in page.
+            // You can specify which fields should be submitted, by adding keys to the `credentials` object.
+            // e.g. domain, username, password, 2FA token, etc.
+            // You can pass any HTML attribute to the <input> tag through the object.
+            credentials: {
+                username: {
+                    label: "아이디",
+                    type: "text",
+                    placeholder: "이메일 주소",
+                },
+                password: { label: "비밀번호", type: "password" },
+            },
+            async authorize(credentials, req) {
+                // Add logic here to look up the user from the credentials supplied
+                const user = {
+                    id: "1",
+                    name: "J Smith",
+                    email: "jsmith@example.com",
+                };
+
+                if (user) {
+                    // Any object returned will be saved in `user` property of the JWT
+                    return user;
+                } else {
+                    // If you return null then an error will be displayed advising the user to check their details.
+                    return null;
+
+                    // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+                }
+            },
         }),
     ],
 });
