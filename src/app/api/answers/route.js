4940922ec2a7cbfac6b1f039e0e516dd01c95820
halionaz <samhsj@naver.com>
2024-03-01 11:38:32 +0900

    ✨ Implement POST Answer API

--- src/app/api/answers/route.js
+++ src/app/api/answers/route.js
@@ -1,10 +1,39 @@
 // api/answers
 
 import { getServerSession } from "next-auth";
-import { authOptions } from "../../auth/[...nextauth]/route";
-import { deleteDoc, doc, getDoc } from "firebase/firestore";
+import { addDoc, collection, deleteDoc, doc, getDoc } from "firebase/firestore";
 import { NextResponse } from "next/server";
 import { db } from "../firebase";
+import { authOptions } from "../auth/[...nextauth]/route";
+
+export async function POST(request){
+    // request로부터 받은 데이터 서버에 추가
+
+    const reqData = await request.json();
+
+    console.log(reqData);
+    const answerVal = reqData.answerVal;
+    const article = reqData.article;
+
+    const session = await getServerSession(authOptions);
+
+    if(session){
+        // 사용자가 정상적으로 로그인 했을 때 DB 등록
+        try {
+            const docRef = await addDoc(collection(db, "answers"), {
+                answerVal,
+                article,
+                respondent: session.user?.email,
+            });
+        } catch (err) {
+            console.error(err);
+        }
+
+        return NextResponse.json("Done It");
+    } else {
+        return NextResponse.error();
+    }
+}
 
 export async function DELETE(request) {
     // 답변 ID를 받아
@@ -24,6 +53,8 @@ export async function DELETE(request) {
             // 답변 작성자와 세션 이메일이 같을 때 삭제 진행
             await deleteDoc(doc(db, "answers", answerID));
         }
+    } else {
+        return NextResponse.error();
     }
 
     return NextResponse.json({ answerID });
