49218e66bb551e20825537e2729ac7cf033b13c5
halionaz <samhsj@naver.com>
2024-03-16 16:13:25 +0900

    🛂 Connect Prisma-NextAuth

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,10 +1,15 @@
 // route.js는 Server-Side API를 위한 문서 작성용
 
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
 import NextAuth from "next-auth/next";
 import GithubProvider from "next-auth/providers/github";
 
+const prisma = new PrismaClient();
+
 const authOptions = {
     secret: process.env.NEXTAUTH_SECRET,
+    adapter: PrismaAdapter(prisma),
     providers: [
         GithubProvider({
             clientId: process.env.GITHUB_ID,
